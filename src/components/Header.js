2ab1126c8f9eb67919945f8f193b509a567ba81b
Ethan <ethandbinnie@gmail.com>
2023-08-14 12:25:53 +1200

    feat: functional header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { HomeIcon, MagnifyingGlassIcon, PlusIcon, StarIcon, FilmIcon, TvIcon } from "@heroicons/react/20/solid"
-
+import { Link } from "react-router-dom";
 
 export default function Header() {
 
@@ -8,22 +8,22 @@ export default function Header() {
       <div className="headerSections">
         <img src="./images/disneylogo.png" alt="logo" className="headerLogo" width={125} height={40}/>
         <div className="headerGenres">
-          <HomeIcon width={20} height={20} className="headerIcon"/> <p>home</p>
+          <HomeIcon width={20} height={20} className="headerIcon"/> <Link to="/" className="link"><p>home</p></Link>
         </div>
         <div className="headerGenres">
-          <MagnifyingGlassIcon width={20} height={20} className="headerIcon"/> <p>search</p>
+          <MagnifyingGlassIcon width={20} height={20} className="headerIcon"/> <Link to="/search" className="link"><p>search</p></Link>
         </div>  
         <div className="headerGenres">
-          <PlusIcon width={20} height={20} className="headerIcon"/> <p>watchlist</p>
+          <PlusIcon width={20} height={20} className="headerIcon"/> <Link to="/watchlist" className="link"><p>watchlist</p></Link>
         </div>
         <div className="headerGenres">
-          <StarIcon width={20} height={20} className="headerIcon"/> <p>originals</p>
+          <StarIcon width={20} height={20} className="headerIcon"/> <Link to="/originals" className="link"><p>originals</p></Link>
         </div>
         <div className="headerGenres">
-          <FilmIcon width={20} height={20} className="headerIcon"/> <p>movies</p>
+          <FilmIcon width={20} height={20} className="headerIcon"/> <Link to="/movies" className="link"><p>movies</p></Link>
         </div>
         <div className="headerGenres">
-          <TvIcon width={20} height={20} className="headerIcon"/> <p>series</p>
+          <TvIcon width={20} height={20} className="headerIcon"/> <Link to="/series" className="link"><p>series</p></Link>
         </div>
       </div>
       <div className="headerPerson">
